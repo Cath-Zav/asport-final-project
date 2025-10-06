070a724c7d2e7b6eedd7a703e11f42a3d43cbbc8
Cath-Zav <125591437+Cath-Zav@users.noreply.github.com>
2025-10-06 02:37:21 +0200

    Merge pull request #3
    * refactor pom.xml

--- src/main/java/by/asport/utils/LoginUtils.java
+++ src/main/java/by/asport/utils/LoginUtils.java
@@ -1,39 +1,40 @@
 package by.asport.utils;
 
 import by.asport.webdriver.WebDriver;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class LoginUtils {
-    private static final String aToZ="abcdefghigklmnopqrstuvwxyz1234567890";
-
-    public static String createRandomWord() {
-        int wordLength = ThreadLocalRandom.current().nextInt(1, 26);
-        StringBuilder randomWord = new StringBuilder();
-        Random rand = new Random();
-
-        for(int i = 0; i < wordLength; i++) {
-            int randIndex = rand.nextInt(aToZ.length());
-            randomWord.append(aToZ.charAt(randIndex));
-        }
-        return randomWord.toString();
-    }
+//    private static final String aToZ="abcdefghigklmnopqrstuvwxyz1234567890";
+//
+//    public static String createRandomWord() {
+//        int wordLength = ThreadLocalRandom.current().nextInt(1, 26);
+//        StringBuilder randomWord = new StringBuilder();
+//        Random rand = new Random();
+//
+//        for(int i = 0; i < wordLength; i++) {
+//            int randIndex = rand.nextInt(aToZ.length());
+//            randomWord.append(aToZ.charAt(randIndex));
+//        }
+//        return randomWord.toString();
+//    }
 
     public static String getRandomEmail() {
-        return createRandomWord() + "%40mail.com";
+        Faker faker = new Faker();
+        return faker.internet().emailAddress();
     }
 
     public static String getRandomPassword() {
-        return createRandomWord();
-    }
-
-    public static void waitUntilLoaded(String xpath) {
-        new WebDriverWait(WebDriver.getDriver(), Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        Faker faker = new Faker();
+        return faker.internet().password();
     }
 }
