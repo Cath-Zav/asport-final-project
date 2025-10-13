7edf3359dd4e0253df1bfc5fcf7dbd63bbf48102
kate <cathzavizion@gmail.com>
2025-10-13 18:36:11 +0200

    refactor code

--- src/main/java/by/asport/utils/CartUtils.java
+++ src/main/java/by/asport/utils/CartUtils.java
@@ -12,6 +12,6 @@ public class CartUtils {
     }
 
     public static String getRandomProductQuant() {
-        return String.valueOf((int) Math.random() * 10000);
+        return String.valueOf((int) (Math.random() * 10000));
     }
 }
